cb2ddc9c90026a205d9e3a2e1378b38162a7eee5
purestmix <jfylucanas@gmail.com>
2016-12-15 06:58:01 +0000

    roll

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,6 +1,4 @@
 class Medicine < ApplicationRecord
     validates :generic , presence: true
-    validates :classifcation  , presence: true
-    validates :medType , presence: true
-    validates :mg , presence: true , numericality: true, length:{minimum:2}
+    validates :mg , presence: true 
 end
